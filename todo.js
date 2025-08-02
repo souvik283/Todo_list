b631534f9592e81ebcab9c151fef76bf13d80c47
Souvik Ghosh <ghosh2005202318@gmail.com>
2025-08-02 08:50:57 +0530

    add

--- todo.js
+++ todo.js
@@ -1,37 +1,37 @@
 let btn = document.getElementById("button")
 let txt = document.getElementById("input-text")
 let txtno = document.getElementById("input-no")
-btn.addEventListener("click",()=>{
+btn.addEventListener("click", () => {
     let text = txt.value.trim()
     let note = txtno.value.trim()
-    if(text && note){
-    localStorage.setItem(note,text)
-    // location.reload()
-    display()
-    }else{
+    if (text && note) {
+        localStorage.setItem(note, text)
+        // location.reload()
+        display()
+    } else {
         alert("Please Fill The Boxes Correctly Your Text")
     }
 })
 
 
 
-window.addEventListener("DOMContentLoaded",()=>{
+window.addEventListener("DOMContentLoaded", () => {
     display()
 })
-let display=()=>{
- let output = document.getElementById("second")
-    let ihtml=""
-for(let i=0; i< localStorage.length ;i++){
-let key1 = localStorage.key(i)
-let value1= localStorage.getItem(key1)
-ihtml += `<div class="content"> <p>${key1} : ${value1}</p>
+let display = () => {
+    let output = document.getElementById("second")
+    let ihtml = ""
+    for (let i = 0; i < localStorage.length; i++) {
+        let key1 = localStorage.key(i)
+        let value1 = localStorage.getItem(key1)
+        ihtml += `<div class="content"> <p>${key1} : ${value1}</p>
 <button onclick="remove('${key1}')" class="del btn">X</button> 
 </div>`
-}
-output.innerHTML= ihtml
+    }
+    output.innerHTML = ihtml
 }
 
-const remove=(key)=>{
+const remove = (key) => {
     localStorage.removeItem(key)
     display()
 
